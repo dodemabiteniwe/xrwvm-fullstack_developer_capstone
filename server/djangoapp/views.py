77ed3ebb94d0f32944e5905da4146dee504fbfdf
dodemabiteniwe <dodemabiteniwe@gmail.com>
2024-05-10 19:23:03 -0400

    new change to github 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -74,8 +74,10 @@ def registration(request):
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
-                        first_name=first_name, last_name=last_name,
-                        password=password, email=email)
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -95,7 +97,7 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name,
-                "CarMake": car_model.car_make.name})
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -151,6 +153,6 @@ def add_review(request):
         except Exception as e:
             print(f"Error: {e}")
             return JsonResponse({"status": 401,
-                           "message": "Error in posting review"})
+                                 "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
